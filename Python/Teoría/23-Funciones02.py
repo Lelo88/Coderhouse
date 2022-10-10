82eadf9a02dc7ece444ae0c63efaf861366f850e
Leandro Villalba <63604403+Lelo88@users.noreply.github.com>
2022-10-10 19:41:31 -0300

    Add files via upload

---
+++ Python/Teoría/23-Funciones02.py
@@ -0,0 +1,6 @@
+def saluda(nombre, apellido):
+    return f'Hola, mi nombre es {nombre} {apellido} '
+
+#si no invoco nada, nada va a pasar nada, porque estoy retornando un valor
+
+saluda("Leandro","Villalba") # si invoco la funcion con los parametros no va a pasar nada,
