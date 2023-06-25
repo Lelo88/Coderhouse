1b25792ad58a6c7f8da9aca8d4bf547d642fe522
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2023-06-24 21:27:47 -0300

    agrego teoria strings

---
+++ Python/Teoría/strings.py
@@ -0,0 +1,13 @@
+nombre_curso = "Python"
+descripcion_curso = """Curso de Python
+Este curso completa todos los detalles que necesitas aprender para encontrar trabajo
+como programador
+"""
+
+print(nombre_curso, descripcion_curso)
+
+print(len(nombre_curso))
+#utilizamos la funcion .upper() y el primer elemento
+print(nombre_curso[0].upper())
+
+print(descripcion_curso[0:5])
