3a85cac81a61e5aca668eaf641cf8af72b9b9189
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2023-06-24 21:27:47 -0300

    agrego teoria strings

--- Python/Teoría/13-Conjuntos.py
+++ Python/Teoría/13-Conjuntos.py
@@ -1,49 +1,50 @@
-#Set heterogéneos 
-mi_var = 'Una variable'
-datos = {1, -5, 123.1,34.32, 'Una cadena', 'Otra cadena', mi_var}
+""""13-MetodosColecciones.py"""
+
+VARIABLE = 'Una variable'
+datos = {1, -5, 123.1, 34.32, 'Una cadena', 'Otra cadena', VARIABLE}
 print(type(datos))
 
-#Las listas son mutables, sin embargo, el set también es mutable, 
+# Las listas son mutables, sin embargo, el set también es mutable,
 # pero no podemos hacer slicing, ni manejar un set por índice.
 
-#conjunto = {'a','b','c'}
-#print(conjunto[:3])
+# conjunto = {'a','b','c'}
+# print(conjunto[:3])
 
-#Funcion add: Agrega un nuevo item al set. Se pueden agregar operaciones matematicsa
-numeros =  {1,2,3,4} 
+# Funcion add: Agrega un nuevo item al set. Se pueden agregar operaciones matematicsa
+numeros = {1, 2, 3, 4}
 numeros.add(5)
 print(numeros)
 
-#Funcion update: Para añadir múltiples elementos a un set se usa la función update(), que 
-# puede tomar como argumento una lista, tupla, string, conjunto o cualquier objeto de tipo iterable. 
+# Funcion update: Para añadir múltiples elementos a un set se usa la función update(), que
+# puede tomar como argumento una lista, tupla, string, conjunto o cualquier objeto de tipo iterable.
 
-numeros =  {1,2,3,4}
-numeros.update([5,6,7,8]) #Agregado de numeros por lista
-numeros.update(range(9,12)) #Agregado de numeros por range
+numeros = {1, 2, 3, 4}
+numeros.update([5, 6, 7, 8])  # Agregado de numeros por lista
+numeros.update(range(9, 12))  # Agregado de numeros por range
 print(numeros)
 
-#Funcion len: mismo uso que en las listas y el los strings
+# Funcion len: mismo uso que en las listas y el los strings
 
 print(len(numeros))
 
-#Funcion discard: elimina el item del set sin modificar el resto del  set
-#Tambien existe la funcion remove, pero la diferencia con discard es que si el elemento a eliminar
-#no se encuentra, entonces la consola nos tira un error
+# Funcion discard: elimina el item del set sin modificar el resto del  set
+# Tambien existe la funcion remove, pero la diferencia con discard es que si el elemento a eliminar
+# no se encuentra, entonces la consola nos tira un error
 
-numeros2 = {1,2,3,4}
+numeros2 = {1, 2, 3, 4}
 print(numeros2)
 
-#in: determinamos si un elemento se encuentra en un conjunto: 
+# in: determinamos si un elemento se encuentra en un conjunto:
 
-print(2 in numeros2) #tira true
+print(2 in numeros2)  # tira true
 
-#clear: sirve para borrar todos los elementos de un conjunto
+# clear: sirve para borrar todos los elementos de un conjunto
 
-#numeros2.clear()
-#print(numeros2)
+# numeros2.clear()
+# print(numeros2)
 
-#pop: en este caso, elimina un elemento de forma aleatoria, ya que en los conjuntos los elementos no
-#estan ordenados
+# pop: en este caso, elimina un elemento de forma aleatoria, ya que en los conjuntos los elementos no
+# estan ordenados
 
 
 while numeros2:
