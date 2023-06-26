904861a64ce1365613bb2ba51ad4327b561e1027
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2023-06-26 20:03:28 -0300

    nuevos llamados de funciones

--- Python/Teoría/23-Funciones02.py
+++ Python/Teoría/23-Funciones02.py
@@ -4,3 +4,17 @@ def saluda(nombre, apellido):
 #si no invoco nada, nada va a pasar nada, porque estoy retornando un valor
 
 saluda("Leandro","Villalba") # si invoco la funcion con los parametros no va a pasar nada,
+
+print(saluda("Leandro","Villalba"))
+
+def hola (nombre, apellido = "Villalba"):
+    return f'hola, mi nombre es {nombre} {apellido} '
+
+hola("Gonzalo") # si invoco la funcion con un parametro, me va a agregar el apellido que se encuentra con default
+
+print(hola("Gonzalo"))
+
+print(hola(apellido="Alvarez", nombre="Gonzalo")) # tambien puedo definir a los argumentos de esta manera
+
+
+
